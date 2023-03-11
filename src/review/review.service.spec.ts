54c09bf1f6d719d57e12a586b4fc2e9304e89e46
AndreiShulha <andreishulha@coherentsolutions.com>
2023-03-11 18:06:18 +0300

    created unit test for review

--- src/review/review.service.spec.ts
+++ src/review/review.service.spec.ts
@@ -1,18 +1,62 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Types } from 'mongoose';
+import { CreateReviewDto } from './dto/create.review.dto';
 import { ReviewService } from './review.service';
 
+const productId = new Types.ObjectId().toHexString();
+const testDto: CreateReviewDto = {
+	name: 'Test',
+	title: 'Title',
+	description: 'test description',
+	rating: 5,
+	productId,
+};
+
+const reviewService = {
+	findByProductId: (id: string) => [testDto],
+};
+
 describe('ReviewService', () => {
-  let service: ReviewService;
+	let service: ReviewService;
+	const mockFn = jest.fn();
+	const reviewRepositoryFactory = () => ({
+		find: mockFn,
+	});
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [
+				ReviewService,
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ReviewService],
-    }).compile();
+				// first variant
+				// {
+				// 	useFactory: reviewRepositoryFactory,
+				// 	provide: getModelToken('ReviewModel'),
+				// },
+			],
+		})
+			// second variant
+			.overrideProvider(ReviewService)
+			.useValue(reviewService)
+			.compile();
 
-    service = module.get<ReviewService>(ReviewService);
-  });
+		service = module.get<ReviewService>(ReviewService);
+	});
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
+
+	it('findByProductId', async () => {
+		const id = new Types.ObjectId().toHexString();
+
+		// first variant
+		// reviewRepositoryFactory().find.mockReturnValueOnce([testDto]);
+		// const res = await service.findByProductId(id);
+
+		// second variant
+		const res = await service.findByProductId(id)
+
+		expect(res?.[0]).toEqual(testDto);
+	});
 });
